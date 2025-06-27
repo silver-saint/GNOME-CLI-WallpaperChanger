c29a499194013c17e44fca77e899df26de3cf2d9
Neyko Naydenov <naydenovneyko@gmail.com>
2025-06-27 17:55:02 +0300

    adding gtest

--- SubmoduleUpdater.sh
+++ SubmoduleUpdater.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ -f .gitmodules ] && [ -s .gitmodules ]; then
     echo "Configuring Submodules..."
     rm -rf ./Vendor
@@ -6,16 +8,13 @@ if [ -f .gitmodules ] && [ -s .gitmodules ]; then
     git submodule update --init --recursive
 else
     echo "No submodules found. Adding."
-    if [-d "./Vendor"]; then
-    echo Creating Folder and adding Submodules.
-    git submodule add https://github.com/google/googletest.git
-    git submodule update --init --recursive
-    else
-    echo adding Submodules
+
+    if [ ! -d "./Vendor" ]; then
+        echo "Creating Folder and adding Submodules."
+        mkdir ./Vendor
+    fi
+
+    cd Vendor
     git submodule add https://github.com/google/googletest.git
     git submodule update --init --recursive
 fi
-
-
-
-
